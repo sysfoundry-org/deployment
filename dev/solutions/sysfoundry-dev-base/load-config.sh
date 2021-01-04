96f719b0a33f9488940ce29045b7a8702b8bb9f4
S00085 <vijayakumar.mohan@accelya.com>
2021-01-04 19:54:24 +0400

    removed \r to fix windows issues

--- dev/solutions/sysfoundry-dev-base/load-config.sh
+++ dev/solutions/sysfoundry-dev-base/load-config.sh
@@ -1,11 +1,8 @@
 #!/bin/sh
 SOLUTION_NAME=$1
-for FILE in /cfg/service/config/*.yml; 
-do 
+for FILE in /cfg/service/config/*.yml;
+do
   FILE_NAME=`basename $FILE .yml`
   echo "Uploading kv for '$FILE_NAME' under solution '$SOLUTION_NAME'"
   curl --request PUT --data-binary @$FILE http://consul:8500/v1/kv/$SOLUTION_NAME/service-config/$FILE_NAME/data
 done
-
-
-
